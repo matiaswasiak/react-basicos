20f2cc82a2f794679301dd1331f57c2183f6f9ad
matiaswasiak <matiaswasiak@gmail.com>
2022-02-10 23:31:09 -0300

    Curso React: 5. Propiedades = jonmircha

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
 import logo from "./logo.svg";
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+        <section>
+          <Componente msg="Hola soy un componente desde una prop" />
+          <hr />
+          <Propiedades
+            cadena="Esto es una cadena de texto"
+            numero={20}
+            boolean={true}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "Matias", correo: "matiaswasiak@gmail.com" }}
+            funcion={(num) => num * num}
+            elementoReact={<i>Esto es un elemento React</i>}
+            componenteReact={
+              <Componente msg="Soy un componente pasado como Prop" />
+            }
+          />
+        </section>
       </header>
-      <section>
-        <Componente msg="Hola soy un componente desde una prop" />
-      </section>
     </div>
   );
 }
---
+++ src/components/Propiedades.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+// const Propiedades = (props) => <h2>Propiedades</h2>;
+// export default Propiedades;
+
+export default function Propiedades(props) {
+  return (
+    <div>
+      {/* <h2>Propiedades</h2> */}
+      <h2>{props.porDefecto}</h2>
+      <ul>
+        <li>{props.cadena}</li>
+        <li>{props.numero}</li>
+        <li>{props.boolean}</li>
+        <li>{props.arreglo.join(", ")}</li>
+        <li>{props.objeto.nombre + " - " + props.objeto.correo}</li>
+        <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+        <li>{props.elementoReact}</li>
+        <li>{props.componenteReact}</li>
+      </ul>
+    </div>
+  );
+}
+
+Propiedades.defaultProps = {
+  porDefecto: "Las Props",
+};
+
+Propiedades.propTypes = {
+  numero: PropTypes.number.isRequired,
+};
