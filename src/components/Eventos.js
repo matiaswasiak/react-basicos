bad2f32a06732a2a1fb29284dbfc0aa93a491f33
matiaswasiak <matiaswasiak@gmail.com>
2022-02-12 00:02:59 -0300

    Curso React: 11. Eventos Nativos, Sintéticos & Per

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -73,3 +73,51 @@ export class EventosES7 extends Component {
     );
   }
 }
+
+// function Boton(props) {
+//   return <button onClick={props.myOnClick}>Botón hecho componente</button>;
+// }
+
+// const Boton = (props) => (
+//   <button onClick={props.myOnClick}>Botón hecho componente</button>
+// );
+
+const Boton = ({ myOnClick }) => (
+  <button onClick={myOnClick}>Botón hecho componente</button>
+);
+
+export class MasSobreEventos extends Component {
+  handleClick = (e, mensaje) => {
+    console.log(e);
+    console.log(e.nativeEvent);
+    console.log(e.target);
+    console.log(e.nativeEvent.target);
+    console.log(mensaje);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Más Sobre Eventos</h2>
+        <button
+          onClick={(e) =>
+            this.handleClick(e, "Hola pasando parámetro desde un evento")
+          }
+        >
+          Saludar
+        </button>
+        {/* Evento Personalizado */}
+        {/* <Boton
+          onClick={(e) =>
+            this.handleClick(e, "Hola pasando parámetro desde un evento")
+          }
+        /> */}
+        <Boton
+          myOnClick={(e) =>
+            this.handleClick(e, "Hola pasando parámetro desde un evento")
+          }
+        />
+      </div>
+    );
+  }
+}
