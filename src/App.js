d281eb762e26b51d2ad6d3008cb6b5921f3390de
matiaswasiak <matiaswasiak@gmail.com>
2022-02-11 23:23:56 -0300

    Curso React: 8. Renderizado de Elementos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Componente from "./components/Componente";
 import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
 
 function App() {
   return (
@@ -43,6 +44,8 @@ function App() {
           <Estado />
           <hr />
           <RenderizadoCondicional />
+          <hr />
+          <RenderizadoElementos />
         </section>
       </header>
     </div>
