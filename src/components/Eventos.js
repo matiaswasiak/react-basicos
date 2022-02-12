f0a59393f4173de6e53a2182b9a1b584ca93f19e
matiaswasiak <matiaswasiak@gmail.com>
2022-02-11 23:44:08 -0300

    Curso React: 10. Eventos & Property Initializers

--- src/components/Eventos.js
+++ src/components/Eventos.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Eventos extends Component {
+export class EventosES6 extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -28,7 +28,42 @@ export default class Eventos extends Component {
   render() {
     return (
       <div>
-        <h2>Eventos en Componentes de Clase</h2>
+        <h2>Eventos en Componentes de Clase ES6</h2>
+        <nav>
+          <button onClick={this.restar}>-</button>
+          <button onClick={this.sumar}>+</button>
+        </nav>
+        <h3>{this.state.contador}</h3>
+      </div>
+    );
+  }
+}
+
+// Properties Initializer
+export class EventosES7 extends Component {
+  state = {
+    contador: 0,
+  };
+
+  // Arrow functions
+  sumar = (e) => {
+    console.log("Sumando");
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  };
+
+  restar = (e) => {
+    console.log("Restando");
+    this.setState({
+      contador: this.state.contador - 1,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Eventos en Componentes de Clase ES7</h2>
         <nav>
           <button onClick={this.restar}>-</button>
           <button onClick={this.sumar}>+</button>
