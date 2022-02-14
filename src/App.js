a3ffb63d2f4f5de4a98f50ae47fb7dfff194b8b7
matiaswasiak <matiaswasiak@gmail.com>
2022-02-13 22:52:46 -0300

    Curso React: 22. Formularios - jonmircha

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import RelojHooks from "./components/RelojHooks";
 import AjaxHooks from "./components/AjaxHooks";
 import HooksPersonalizados from "./components/HooksPersonalizados";
 import Referencias from "./components/Referencias";
+import Formularios from "./components/Formularios";
 
 function App() {
   return (
@@ -80,6 +81,8 @@ function App() {
           <HooksPersonalizados />
           <hr />
           <Referencias />
+          <hr />
+          <Formularios />
         </section>
       </header>
     </div>
